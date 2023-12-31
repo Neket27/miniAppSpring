f721b3fe64b90274a2767ae8094e2f71195077fa
neket <kulganikitaivanovich@yandex.ru>
2023-12-31 16:10:48 +0300

    init

---
+++ src/main/java/app/miniappspring/MiniAppSpringApplication.java
@@ -0,0 +1,13 @@
+package app.miniappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAppSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/miniappspring/MiniAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package app.miniappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
