2b40b24ff36f073b542723ba48c808381991733a
neket <kulganikitaivanovich@yandex.ru>
2024-01-01 14:20:53 +0300

    init

--- src/main/java/app/miniappspring/controller/GreetingController.java
+++ src/main/java/app/miniappspring/controller/GreetingController.java
@@ -1,7 +1,6 @@
 package app.miniappspring.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,7 +12,6 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String,Object> model) {
      model.put("name",name);
-
         return "greeting";
     }
 
