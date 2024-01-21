fe570dd7172736b9a20937fd2c33e1a583c486e1
neket <kulganikitaivanovich@yandex.ru>
2024-01-22 01:51:55 +0300

    creating base authentication

--- react-app/src/auth/store/Store.ts
+++ react-app/src/auth/store/Store.ts
@@ -1,5 +1,4 @@
 import {IUser} from "../model/IUser";
-// @ts-ignore
 import AuthService from "../service/AuthService";
 import { makeAutoObservable} from "mobx";
 import axios from "axios";
@@ -30,11 +29,9 @@ export default class Store{
         try{
             const response =await AuthService.login(username,password);
             console.log(response);
-
-            // @ts-ignore
-            localStorage.getItem('token',response.data.accessToken);
+            localStorage.setItem("token",response.data.accessToken);
             this.setAuth(true);
-            // this.setUser(response.data.user);
+            this.setUser(response.data.user);
             let user:IUser={
                 id:'1',
                 email:'nik@gmail.com',
@@ -52,8 +49,7 @@ export default class Store{
         try{
             const response =await AuthService.registration(username,password);
             console.log(response);
-            // @ts-ignore
-            localStorage.getItem('token',response.data.accessToken);
+            localStorage.setItem('token',response.data.accessToken);
             this.setAuth(true);
             // this.setUser(response.data.user);
             let user:IUser={
@@ -72,7 +68,6 @@ export default class Store{
         try{
             const response =await AuthService.logout();
             console.log(response);
-            // @ts-ignore
             localStorage.removeItem('token');
             this.setAuth(false);
             this.setUser({} as IUser);
@@ -82,17 +77,51 @@ export default class Store{
         }
     }
 
+    async resetPassword(password:string,newPassword:string){
+        try {
+            const response =await AuthService.resetPassword(password,newPassword);
+            console.log(response);
+
+            localStorage.setItem("token",response.data.accessToken);
+            this.setAuth(true);
+            this.setUser(response.data.user);
+            let user:IUser={
+                id:'1',
+                email:'nik@gmail.com',
+                isActivated:true,
+            };
+
+            this.setUser(user);
+        }catch (e){
+            // @ts-ignore
+            console.log(e.response?.data?.message);
+        }
+    }
+
+    //если пользователь авторизован, то у него будет сохранен refresh токен в cooke
+    //пока срок годности refresh токена не истек, можно запросить новую пару токенов у сервера
+    //иначе: localStorage.setItem('token',response.data.accessToken); установится в null
+    //и в другом месте можно будет проверить авторизацию пользователя сделав   if(localStorage.getItem('token')) и в этом условии вызвать checkAuth()
     async checkAuth(){
         this.setLoading(true);
         try {
-            const response = await axios.get<AuthResponse>(`${API_URL}/api/v1/auth/refresh`,{withCredentials:true});
+           // const response = await axios.post<AuthResponse>(`${API_URL}/api/v1/auth/refresh`,{withCredentials:true});
+            const response = await AuthService.refresh();
             console.log(response);
+            console.log("------------------------");
             localStorage.setItem('token',response.data.accessToken);
             this.setAuth(true);
-            this.setUser(response.data.user);
+            // this.setUser(response.data.user);
+            let user:IUser={
+                id:'1',
+                email:'nik@gmail.com',
+                isActivated:true,
+            };
+            this.setUser(user);
             }catch (e){
             // @ts-ignore
             console.log(e.response?.data?.message);
+            console.log("Пользователь не авторизован или срок действия refresh токена истёк, пройдите авторизацию заново");
         }finally {
             this.setLoading(false);
         }
