8a77eae9301f914b1052acd220a2648cbea95907
neket <nikitaivanovitc@gmail.com>
2024-07-14 21:50:12 +0300

    remove excess

---
+++ backend/target/generated-sources/annotations/app/miniappspring/utils/jwtToken/mapper/CartMapperImpl.java
@@ -0,0 +1,46 @@
+package app.miniappspring.utils.jwtToken.mapper;
+
+import app.miniappspring.dto.cart.CreateProductCartDto;
+import app.miniappspring.dto.cart.ProductCartDto;
+import app.miniappspring.entity.BagProduct;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-07-14T16:03:18+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (Amazon.com Inc.)"
+)
+@Component
+public class CartMapperImpl implements CartMapper {
+
+    @Override
+    public BagProduct toProductCart(CreateProductCartDto createProductCartDto) {
+        if ( createProductCartDto == null ) {
+            return null;
+        }
+
+        BagProduct bagProduct = new BagProduct();
+
+        bagProduct.setIdProduct( createProductCartDto.getIdProduct() );
+        bagProduct.setCount( createProductCartDto.getCount() );
+        bagProduct.setShowInCart( createProductCartDto.isShowInCart() );
+
+        return bagProduct;
+    }
+
+    @Override
+    public ProductCartDto toCarProductDto(BagProduct cartProduct) {
+        if ( cartProduct == null ) {
+            return null;
+        }
+
+        ProductCartDto productCartDto = new ProductCartDto();
+
+        productCartDto.setIdProduct( cartProduct.getIdProduct() );
+        productCartDto.setCount( cartProduct.getCount() );
+        productCartDto.setShowInCart( cartProduct.isShowInCart() );
+
+        return productCartDto;
+    }
+}
---
+++ backend/target/generated-sources/annotations/app/miniappspring/utils/jwtToken/mapper/CharacteristicMapperImpl.java
@@ -0,0 +1,30 @@
+package app.miniappspring.utils.jwtToken.mapper;
+
+import app.miniappspring.dto.characteristic.CharacteristicProductDto;
+import app.miniappspring.entity.CharacteristicProduct;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-07-14T16:03:18+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (Amazon.com Inc.)"
+)
+@Component
+public class CharacteristicMapperImpl implements CharacteristicMapper {
+
+    @Override
+    public CharacteristicProductDto toCharacteristicProductDto(CharacteristicProduct characteristicProduct) {
+        if ( characteristicProduct == null ) {
+            return null;
+        }
+
+        CharacteristicProductDto.CharacteristicProductDtoBuilder characteristicProductDto = CharacteristicProductDto.builder();
+
+        characteristicProductDto.id( characteristicProduct.getId() );
+        characteristicProductDto.producerCountry( characteristicProduct.getProducerCountry() );
+        characteristicProductDto.sellerWarranty( characteristicProduct.getSellerWarranty() );
+
+        return characteristicProductDto.build();
+    }
+}
